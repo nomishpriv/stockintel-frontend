6308cb4060bc1bcbb3f89284bf60d3c3f0521f36
nomishpriv <nomanwahid123@gmail.com>
2026-05-17 14:05:35 +0500

    Update API URL for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  // baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://saddlebrown-scorpion-534823.hostingersite.com/api',
   timeout: 10000
 });
 
